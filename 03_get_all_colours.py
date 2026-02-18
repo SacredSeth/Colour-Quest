79323fda587c1b1ab836eb53659b8f41f26d387a
SacredSeth <141595269+SacredSeth@users.noreply.github.com>
2026-02-19 10:21:51 +1300

    Game layout (placeholder colours)

---
+++ 03_get_all_colours.py
@@ -0,0 +1,10 @@
+import csv
+import random
+
+# retrieve colours from csv file and put them in a list
+file = open("00_colour_list_hex_v3.csv", "r")
+all_colors = list(csv.reader(file, delimiter=","))
+file.close()
+
+# remove the first row
+all_colors.pop(0)
